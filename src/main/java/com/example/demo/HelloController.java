2e9a6beb8fa10e1772b3dd8dcba9d3e5665419c5
Trần Phúc Mạnh Linh <linh2501@outlook.com.vn>
2022-04-12 17:22:14 +0700

    New change

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,23 @@
 package com.example.demo;
 
+import java.lang.String;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextField nameInput;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    @FXML
+    protected void onImportButtonClick() {
+        String s = nameInput.getText();
+        welcomeText.setText(s);
     }
 }
